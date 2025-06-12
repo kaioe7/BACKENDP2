eed83572986a788f690f3ec18218dcd536b9aaec
kaio oliveira <email@aluno.com>
2025-06-12 20:11:55 -0300

    erro na views e url

--- projeto/todos/views.py
+++ projeto/todos/views.py
@@ -1,17 +1,22 @@
 from django.shortcuts import render
+
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView
 from .models import Todo
+
 # Create your views here.
+
 class TodoListView(ListView):
     model = Todo
 
+
 class TodoCreateView(CreateView):
-     model = Todo
+    model = Todo
     fields = ['title','deadline']
     success_url = reverse_lazy('todo_list')
 
+
 class TodoUpdateView(UpdateView):
-    model = Todo
-    fields = ['title', 'deadline']
-    success_url = reverse_lazy('todo_list')
+       model = Todo
+       fields = ['title', 'deadline']
+       success_url = reverse_lazy('todo_list')
